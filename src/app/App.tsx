d34af84a3df6f00f489ecc831df26bbffba55c31
Denis Sitnikov <equi.denis@gmail.com>
2024-01-11 20:49:19 +0100

    feat: added navbar component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,16 +6,16 @@ import { useTheme } from 'app/providers/ThemeProvider';
 import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
 import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={'/'}>Main</Link>
-      <Link to={'/about'}>About us</Link>
+      <Navbar />
       <AppRouter />
+      <button onClick={toggleTheme}>TOGGLE</button>
     </div>
   );
 };
