63759ab6f60a8dc9af6d29898a1959d26b0695b3
Denis Sitnikov <equi.denis@gmail.com>
2024-01-28 13:36:32 +0100

    chore: moved switcher to sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -16,6 +17,9 @@ export const Sidebar = ({ className }: SidebarProps) => {
   return (
     <div className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
       <button onClick={onToggle}>toggle</button>
+      <div className={cls.switchers}>
+        <ThemeSwitcher />
+      </div>
     </div>
   );
 };
